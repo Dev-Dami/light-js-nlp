b3786679e9fb3024bc805d4e0ef941975dde8312
Damilare Osibanjo <141376183+Dev-Dami@users.noreply.github.com>
2025-11-09 00:24:40 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export { Pipeline } from "./core/pipeline";
 export { Tokenizer } from "./core/Tokenizer";
 export * from "./processors";
+export * from "./adapters";
 export type { IntentResult, Entity } from "./types";
+export type { MiniparseConfig } from "./config/defaults";
+export { ConfigLoader } from "./config/loader";
